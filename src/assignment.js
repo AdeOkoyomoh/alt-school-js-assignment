11df603ba5b8bd0dbc9e43c7d734cf1ec5aabcf0
Okoyomoh Adenomoh <okoyomohadenomoh@gmail.com>
2022-07-14 15:55:42 +0100

    Attempt first and second challemges

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sumOfArray;
+    for (let i = 0; i < arrayOfNumbers.length; 1++) {
+        sumOfArrays += arrayOfNumbers[i];
+    }
+    return sumOfArray;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let result = arrayOfNumbers.filter(isEven);
+
+    let isEven = number => {
+        if (number % 2 === 0) {
+            return number;
+        }
+    }
+    
+    return result;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
